465304e8c0a7de11af98e62e10060fb9fc3c4e4f
Twanska <91396530+Twanska@users.noreply.github.com>
2021-09-29 16:37:26 +0200

    Update Wintersportvakanties.py

--- Wintersportvakanties.py
+++ Wintersportvakanties.py
@@ -21,7 +21,8 @@ import streamlit as st
 
 
 # In[3]:
-
+st.title('Case 2 - Wintersportvakanties 1989 - 2016')
+st.title('Toine Smulders, Gabriella Tegov, Kim Nap')
 
 # Assign URL to variable: url
 url = 'https://opendata.cbs.nl/ODataApi/odata/37282/TypedDataSet'
